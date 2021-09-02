288d33fb39b722014c966fa5681cb7f29a7446ea
YangSeungwon <yysw1109@gmail.com>
2021-09-02 16:47:02 +0000

    mesh works for weight stationary

--- src/main/mesh.hpp
+++ src/main/mesh.hpp
@@ -30,6 +30,227 @@ void Mesh<INPUT_T, OUTPUT_T, ACC_T>::calculate()
     assert(waiting.size() > 0);
     auto *cur = &waiting[0];
 
+    clock += 1;
+
+    /* filling input_a of the leftmost tiles in this mesh */
+    uint32_t i = 0;
+    INPUT_T tmp_a[cur->size];
+    if (cur->control.stationary == Stationary::weight)
+    {
+        if (cur->preload < cur->size) // preloading weight, not ready for input
+        {
+            for (; i < cur->size; i++)
+                tmp_a[i] = 0;
+        }
+        else if (cur->progress < cur->size)
+        {
+            for (; i < cur->progress + 1; i++)
+                tmp_a[i] = cur->in_a->get(cur->progress - i, i);
+
+            for (; i < cur->size; i++)
+                tmp_a[i] = 0;
+        }
+        else if (cur->progress < 2 * cur->size)
+        {
+            if (waiting.size() > 1 
+                && waiting[1].progress < waiting[1].size 
+                && waiting[1].control.stationary == Stationary::weight) // more calc waiting
+            {
+                auto *next = &waiting[1];
+                for (; i < next->progress + 1; i++)
+                    tmp_a[i] = next->in_a->get(next->progress - i, i);
+            }
+            for (; i < cur->progress - cur->size + 1; i++)
+                tmp_a[i] = 0;
+
+            for (; i < cur->size; i++)
+                tmp_a[i] = cur->in_a->get(cur->progress - i, i);
+        }
+        else
+        {
+            if (waiting.size() > 1 
+                && waiting[1].progress < 2 * waiting[1].size
+                && waiting[1].control.stationary == Stationary::weight) // more calc waiting
+            {
+                auto *next = &waiting[1];
+                for (; i < next->progress - next->size + 1; i++)
+                    tmp_a[i] = 0;
+
+                for (; i < next->size; i++)
+                    tmp_a[i] = next->in_a->get(next->progress - i, i);
+            }
+            
+            for (; i < cur->size; i++)
+                tmp_a[i] = 0;
+        }
+    }
+    for (uint32_t i_tile = 0; i_tile < rows; i_tile++)
+        tile_table(i_tile,0).input_a(&tmp_a[tile_rows * i_tile]);
+
+
+
+    /* filling input_b and input_d of the uppermost tiles in this mesh */
+    uint32_t j = 0;
+    INPUT_T tmp_b[cur->size];
+    INPUT_T tmp_d[cur->size];
+    control_t tmp_ctrl[cur->size];
+    control_t ctrl_default = {waiting.back().control.stationary, !waiting.back().control.propagate};
+    if (cur->control.stationary == Stationary::weight)
+    {
+        if (cur->preload < cur->size) // preloading weight
+        {
+            for (; j < cur->preload + 1; j++) {
+                tmp_b[j] = cur->in_b->get(cur->size - cur->preload + j - 1, j);
+                tmp_d[j] = cur->in_d->get(cur->size - cur->preload + j - 1, j);
+                tmp_ctrl[j] = cur->control;
+            }
+            for (; j < cur->size; j++) {
+                tmp_b[j] = 0;
+                tmp_d[j] = 0;
+                tmp_ctrl[j] = ctrl_default;
+            }
+        }
+        else if (cur->progress < cur->size)
+        {
+            if (waiting.size() > 1 
+                && waiting[1].preload < waiting[1].size
+                && waiting[1].control.stationary == Stationary::weight) // more calc waiting
+            {
+                auto *next = &waiting[1];
+                for (; j < next->preload + 1; j++) {
+                    tmp_b[j] = next->in_b->get(next->size - next->preload + j - 1, j);
+                    tmp_d[j] = next->in_d->get(next->size - next->preload + j - 1, j);
+                    tmp_ctrl[j] = next->control;
+                }
+            }
+            for (; j < cur->progress + 1; j++) {
+                tmp_b[j] = 0;
+                tmp_d[j] = 0;
+                tmp_ctrl[j] = ctrl_default;
+            }
+
+            for (; j < cur->size; j++) {
+                tmp_b[j] = cur->in_b->get(j - cur->progress - 1, j);
+                tmp_d[j] = cur->in_d->get(j - cur->progress - 1, j);
+                tmp_ctrl[j] = cur->control;
+            }
+        }
+        else
+        {
+            if (waiting.size() > 1 
+                && waiting[1].progress < waiting[1].size
+                && waiting[1].control.stationary == Stationary::weight) // more calc waiting
+            {
+                auto *next = &waiting[1];
+                for (; j < next->progress + 1; j++) {
+                    tmp_b[j] = 0;
+                    tmp_d[j] = 0;
+                    tmp_ctrl[j] = ctrl_default;
+                }
+
+                for (; j < next->size; j++) {
+                    tmp_b[j] = next->in_b->get(j - next->progress - 1, j);
+                    tmp_d[j] = next->in_d->get(j - next->progress - 1, j);
+                    tmp_ctrl[j] = next->control;
+                }
+            }
+            for (; j < cur->size; j++) {
+                tmp_b[j] = 0;
+                tmp_d[j] = 0;
+                tmp_ctrl[j] = ctrl_default;
+            }
+        }
+    }
+    for (uint32_t j_tile = 0; j_tile < columns; j_tile++)
+    {
+        tile_table(0,j_tile).input_b(&tmp_b[tile_columns * j_tile]);
+        tile_table(0,j_tile).input_d(&tmp_d[tile_columns * j_tile]);
+        tile_table(0,j_tile).input_ctrl(&tmp_ctrl[tile_columns * j_tile]);
+    }
+
+    /* filling input_a, input_b and input_d of the other tiles */
+    for (int32_t i = rows-1; i >= 0; i--)
+    {
+        for (int32_t j = columns-1; j >= 0; j--)
+        {
+            if (j > 0)
+            {
+                tile_table(i,j).input_a(tile_table(i, j-1).out_a);
+            }
+            if (i > 0)
+            {
+                tile_table(i,j).input_b(tile_table(i-1, j).out_b);
+                tile_table(i,j).input_d(tile_table(i-1, j).out_c);
+                tile_table(i,j).input_ctrl(tile_table(i-1, j).out_ctrl);
+            }
+
+            tile_table(i,j).calculate();
+        }
+    }
+
+
+    // increase state
+
+    if (cur->preload < cur->size)
+        cur->preload += 1;
+    else {
+        cur->progress += 1;
+        if (waiting.size() > 1)
+        {
+            if (waiting[1].control.stationary == Stationary::weight)
+            {
+                if (waiting[1].preload < waiting[1].size)
+                    waiting[1].preload += 1;
+                else
+                    waiting[1].progress += 1;
+            }
+        }
+    }
+
+
+    std::cout << "[Curr] Preload: " << cur->preload << "\tProgress: " << cur->progress << "\n";
+    if (waiting.size() > 1)
+        std::cout << "[Next] Preload: " << waiting[1].preload << "\tProgress: " << waiting[1].progress << "\n";
+    std::cout << '\n';
+
+    // getting output
+
+    if (cur->control.stationary == Stationary::weight)
+    {
+        if (waiting.size() > 1 && waiting[1].progress >= waiting[1].size)
+        {
+            auto *next = &waiting[1];
+            for (uint32_t i=0; i<next->progress - next->size + 1; i++)
+            {
+                uint32_t x = next->progress - next->size - i;
+                if (x >= next->size) continue;
+                if (i >= next->size) continue;
+                next->out_b->get(x, i) = tile_table(rows-1, i/tile_rows).out_b[i%tile_rows];
+                std::cout << "[Next] Output : " << x << " " << i << " - " << next->out_b->get(x, i) << '\n';
+            }
+        }
+
+        if (cur->progress >= cur->size)
+        {
+            uint32_t cnt = 0;
+            for (uint32_t i=0; i<cur->progress - cur->size + 1; i++)
+            {
+                uint32_t x = cur->progress - cur->size - i;
+                if (x >= cur->size) continue;
+                if (i >= cur->size) continue;
+                cnt++;
+                cur->out_b->get(x, i) = tile_table(rows-1, i/tile_rows).out_b[i%tile_rows];
+                std::cout << "[Curr] Output : " << x << " " << i << " - " << cur->out_b->get(x, i) << '\n';
+            }
+
+            if (cnt == 1 && cur->progress > cur->size){
+                std::cout << "[+] end of a calculation.\n";
+                std::cout << "   consumed clock from the beginning: " << clock << '\n';
+                *(cur->finished) = true;
+                waiting.erase(waiting.begin());
+            }
+        }
+    }
 }
 
 template <typename INPUT_T, typename OUTPUT_T, typename ACC_T>
